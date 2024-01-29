99167db13de3ba5f0a297f66285e658d0fa2678e
Mahmudul Hassan <mahmudul.hubject@gmail.com>
2024-01-29 15:54:20 +0100

    updated to angular 15

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,7 +4,7 @@ import {UserLayoutComponent} from "./layout/user-layout/user-layout.component";
 import {AuthLayoutComponent} from "./layout/auth-layout/auth-layout.component";
 import {HeaderComponent} from "./layout/partial/header/header.component";
 import {SidebarComponent} from "./layout/partial/sidebar/sidebar.component";
-import {RouterLinkActive, RouterLinkWithHref, RouterOutlet} from "@angular/router";
+import {RouterLinkActive, RouterLink, RouterOutlet} from "@angular/router";
 import {NgbCollapseModule, NgbDropdownModule} from "@ng-bootstrap/ng-bootstrap";
 import {CommonModule} from "@angular/common";
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
@@ -15,7 +15,7 @@ import {FileSizePipe} from "./pipe/file-size-pipe";
         CommonModule,
         RouterOutlet,
         RouterLinkActive,
-        RouterLinkWithHref,
+        RouterLink,
         NgbCollapseModule,
         FontAwesomeModule,
         NgbDropdownModule
